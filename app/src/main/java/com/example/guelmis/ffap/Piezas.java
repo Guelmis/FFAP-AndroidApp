44b0a0ea122f11052b7606db63b6651b7ab50377
Mario J. Lara <mario.lara1289@gmail.com>
2015-08-02 20:24:58 -0400

    Identificando algo que necesita cambiar

--- app/src/main/java/com/example/guelmis/ffap/Piezas.java
+++ app/src/main/java/com/example/guelmis/ffap/Piezas.java
@@ -61,6 +61,7 @@ public class Piezas extends ActionBarActivity {
                 startActivity(myIntent);
             }
         });
+	//Esto necesita cambios
         carrito.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
