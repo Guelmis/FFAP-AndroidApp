de4a96d8b4a4f9593e2a0424faa5b25778209c45
Guelmis <guelmisedwards@gmail.com>
2015-10-30 21:27:10 -0400

    Creada funcionalidad para LogOut, creado activity para ver las ordens y vehiculos registrados, pequenos cambios en la parte estetica. Version revisada

---
+++ app/src/main/java/com/example/guelmis/ffap/Vehiculos.java
@@ -0,0 +1,75 @@
+package com.example.guelmis.ffap;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+public class Vehiculos extends ActionBarActivity {
+    ListView ListaVehiculos;
+    private String usuario;
+    ActionBar actionbar;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vehiculos);
+        actionbar = getSupportActionBar();
+        actionbar.setDisplayShowHomeEnabled(true);
+        actionbar.setTitle("FFAP Vehículos Registrados");
+        actionbar.setIcon(R.mipmap.ffap);
+        Intent myIntent = getIntent();
+        usuario = myIntent.getStringExtra("usuario");
+    }
+    @Override
+     public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            // Caso
+            case R.id.id_home:
+                Intent intent  = new Intent(this, Home.class);
+                intent.putExtra("usuario", usuario);
+                startActivity(intent);
+                return true;
+            case R.id.id_carrito:
+                Intent myIntent  = new Intent(this, Carrito.class);
+                myIntent.putExtra("usuario", usuario);
+                startActivity(myIntent);
+                return true;
+            case R.id.id_ordenes:
+                Intent intent1  = new Intent(this, Ordenes.class);
+                intent1.putExtra("usuario", usuario);
+                startActivity(intent1);
+                return true;
+            case R.id.id_vehiculos:
+                Intent intent2  = new Intent(this, Vehiculos.class);
+                intent2.putExtra("usuario", usuario);
+                startActivity(intent2);
+                return true;
+            case R.id.id_chassis:
+                Intent intent3  = new Intent(this, Chassis.class);
+                intent3.putExtra("usuario", usuario);
+                startActivity(intent3);
+                return true;
+            case R.id.id_logout:
+                finishAffinity();
+                startActivity(new Intent(this, MainActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
